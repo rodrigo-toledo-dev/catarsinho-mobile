c17141f45dfa58ac29a6c946f4c4544b67ac3b99
Rodrigo Toledo <rodrigo.toledo.dev@gmail.com>
2019-06-13 19:16:00 -0300

    fix the ip just for the video

--- src/components/CardProject.js
+++ src/components/CardProject.js
@@ -17,9 +17,10 @@ const ProjectImageView = styled.View`
   justify-content: center;
 `;
 
-const ProjectImage = styled.Image.attrs({
-  source: {uri: 'https://storage.googleapis.com/catarsinho/vcEwbhfgZKaHQTMqgRq16fkd?GoogleAccessId=catarsinho%40catarsinho.iam.gserviceaccount.com&Expires=1560432604&Signature=YVgYbjhxwtFUg00qA5YT3FGCfUz0OCI2q7E%2FvLAm6aLWgIkiTHRIaRuqKFJiSQik%2BvRgw2TlBHnxXxutMki4lK1lUwWFFDgBem5GToUe6Hs5Y3LRfhzhn2a57g8MloyEITCMG0Ro1JoITXqJWkqEIjjytDceCTWSBNbC2353pkjKnO74ZIcns42r8L5rf8qiS6E0xpY1%2FoGpzm%2F%2F7P9YWqXmf7V0JlmB2d5Iv4DIt%2Bm5tU0AsW%2Bi%2BnMlw6X6REYCZDrpCrFkrwHXnsqu%2FjnZ5PWShXfAm0cjMLNVYRcjHqaKY4fTob%2BHZlTweY3y9RZbDQcBguItYZIlkkMFksbRLg%3D%3D&response-content-disposition=inline%3B+filename%3D%22the-witcher-iii-wild-hunt.png%22%3B+filename%2A%3DUTF-8%27%27the-witcher-iii-wild-hunt.png&response-content-type=image%2Fpng'}
-})`
+const ProjectImage = styled.Image.attrs(props => ({
+  source: {uri: props.imageSource}
+}))
+`
   min-width: 150px;
   min-height: 150px;
   margin-top: 20px;
@@ -47,7 +48,7 @@ export default class CardProject extends Component {
     return (
       <ProjectView key={this.props.project.slug}>
         <ProjectImageView>
-          <ProjectImage />
+          <ProjectImage imageSource={this.props.project.image} />
         </ProjectImageView>
         <StrongText>{this.props.project.title}</StrongText>
         <Text>Valor a arrecadar R$ {this.props.project.value_to_get}</Text>
