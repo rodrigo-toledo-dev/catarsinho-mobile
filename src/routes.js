ec6fa840e92abb74b129346d84afc6a62a6cd779
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-06-15 20:00:37 -0300

    fix the routing problems using the right element try use flatlist

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import Main from '~/pages/Main';
 
-const Routes = createAppContainer(createSwitchNavigator({ Main }));
+const Routes = createAppContainer(createStackNavigator({ Main: { screen: Main,
+  navigationOptions: () => ({ header: null })
+},
+}));
 
 export default Routes;
